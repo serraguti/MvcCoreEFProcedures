aa5152ed9ce0933ed21d998f2a02904a68bc0d77
serraguti <46522403+serraguti@users.noreply.github.com>
2022-01-14 14:23:46 +0100

    commit

--- MvcCoreEFProcedures/Data/EnfermosContext.cs
+++ MvcCoreEFProcedures/Data/EnfermosContext.cs
@@ -5,6 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MvcCoreEFProcedures.Models;
 
+#region VISTAS SQL SERVER
+//create view EMPLEADOS_DEPARTAMENTOS
+//AS
+//select ISNULL(EMP.EMP_NO, 0) AS EMP_NO
+//	, EMP.APELLIDO, EMP.OFICIO
+//	, DEPT.DNOMBRE AS DEPARTAMENTO
+//	, DEPT.LOC AS LOCALIDAD
+//from EMP 
+//inner join DEPT
+//ON EMP.DEPT_NO = DEPT.DEPT_NO
+#endregion
+
 namespace MvcCoreEFProcedures.Data
 {
 	public class EnfermosContext: DbContext
---
+++ MvcCoreEFProcedures/Models/VistaEmpleado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreEFProcedures.Models
+{
+    [Table("EMPLEADOS_DEPARTAMENTOS")]
+    public class VistaEmpleado
+    {
+        [Key]
+        [Column("EMP_NO")]
+        public int IdEmpleado { get; set; }
+        [Column("APELLIDO")]
+        public String Apellido { get; set; }
+        [Column("OFICIO")]
+        public String Oficio { get; set; }
+        [Column("DEPARTAMENTO")]
+        public String Departamento { get; set; }
+        [Column("LOCALIDAD")]
+        public String Localidad { get; set; }
+    }
+}
